006ff3381cc94b4a261967a7c9a6d9f8003f769c
Reidwids <reidwids@gmail.com>
2022-09-27 13:22:57 -0400

    blockchain python conversions to js

--- models/Blockchain.ts
+++ models/Blockchain.ts
@@ -11,7 +11,7 @@ export class Blockchain {
     this.chain = [];
     this.transactions = [];
     this.createBlock(1, '0');
-    this.nodes = new Set<string>();
+    this.nodes = new Set();
   }
 
   createBlock(this: any, proof: number, previousHash: string): Block {
@@ -28,7 +28,7 @@ export class Blockchain {
   }
 
   getPreviousBlock(this: any) {
-    return this.chain[-1];
+    return this.chain[this.chain.length - 1];
   }
 
   proofOfWork(this: any, previousProof: number) {
@@ -56,26 +56,25 @@ export class Blockchain {
   }
 
   isChainValid(this: any, chain: Block[]) {
-    let previousBlock = chain[0];
-    let blockIndex = 1;
-    while (blockIndex < chain.length) {
-      let block = chain[blockIndex];
-      if (block.previousHash != this.hash(previousBlock)) {
-        return false;
+    chain.forEach((prevBlock, i) => {
+      let block = chain[i + 1];
+      if (block) {
+        if (block.previousHash != this.hash(prevBlock)) {
+          return false;
+        }
+        let previousProof = prevBlock.proof;
+        let proof = block.proof;
+        let hashOperation = crypto
+          .createHash('sha256')
+          .update((proof ** 2 - previousProof ** 2).toString())
+          .digest('hex');
+        if (hashOperation.slice(0, 4) != '0000') {
+          return false;
+        }
       }
-      let previousProof = previousBlock.proof;
-      let proof = block.proof;
-      let hashOperation = crypto
-        .createHash('sha256')
-        .update((proof ** 2 - previousProof ** 2).toString())
-        .digest('hex');
-      if (hashOperation.slice(0, 4) != '0000') {
-        return false;
-      }
-      previousBlock = block;
-      blockIndex += 1;
-      return true;
-    }
+    });
+
+    return true;
   }
 
   addTransaction(this: any, sender: string, receiver: string, amount: string) {
@@ -90,7 +89,9 @@ export class Blockchain {
 
   addNode(this: any, address: string) {
     let parsedUrl = new URL(address);
-    this.nodes.add(parsedUrl.hostname);
+    console.log(parsedUrl.host);
+    this.nodes.add(parsedUrl.host);
+    console.log(this.nodes);
   }
 
   async replaceChain(this: any) {
